501d17d7fb2ab0925252e46357f9fa20831ad545
Kennedy Vieira <kennedy.psic@gmail.com>
2024-02-05 23:03:08 -0300

    Update

--- PrincipioResponsabilidadeSolid/src/principio/responsabilidade/ContaBancaria.java
+++ PrincipioResponsabilidadeSolid/src/principio/responsabilidade/ContaBancaria.java
@@ -1,32 +1,33 @@
 package principio.responsabilidade;
 
 public class ContaBancaria {
-	
-private String descricao;
-	
+
+	private String descricao;
+
 	private double saldo = 8000;
-	
+
 	public void soma100Reais() {
 		saldo += 100;
-		
+
 	}
-	
+
 	public void diminui100Reais() {
 		saldo -= 100;
 	}
-	
+
 	public void sacarDinheiro(double saque) {
 		saldo -= saque;
 	}
-	
+
 	public void depositoDinheiro(double deposito) {
 		saldo += deposito;
-		
+
 	}
-	
+
 	public double getSaldo() {
 		return saldo;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
@@ -34,6 +35,6 @@ private String descricao;
 	@Override
 	public String toString() {
 		return "ContaBancaria [descricao=" + descricao + ", saldo=" + saldo + "]";
-    
 
+	}
 }
